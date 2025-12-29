5343ef0864fd6f6107d82ca25ebbe3074a5bed47
git-hub-geek-dot <2305203.rcca@gmail.com>
2025-12-29 21:31:25 +0530

    Delete VolunteerX/routes directory

--- VolunteerX/routes/eventRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authenticateToken = require("../middleware/auth");
-const eventController = require("../controllers/eventController");
-
-router.post("/events", authenticateToken, eventController.createEvent);
-router.get("/events", eventController.getAllEvents);
-
-module.exports = router;
--- VolunteerX/routes/ratingRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authenticateToken = require("../middleware/auth");
-const controller = require("../controllers/ratingController");
-
-router.post("/ratings", authenticateToken, controller.giveRating);
-router.get("/ratings/:id", controller.getRatingsForUser);
-
-module.exports = router;
